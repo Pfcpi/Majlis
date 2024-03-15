febe16dedf5a726c5386603dc7569654e87cddaf
Amirmadjour <amirmadjour133@gmail.com>
2024-03-15 11:47:00 +0100

    remove flon layout

---
+++ src/renderer/src/components/Flon.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './components_css/comp.css'
+
+function Flon() {
+  return (
+    <label class="switch">
+      <input type="checkbox" />
+      <span class="slider round"></span>
+    </label>
+  )
+}
+export default Flon
