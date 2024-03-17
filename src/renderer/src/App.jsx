65693ad674bb735021e7b090728782e18c288403
Amirmadjour <amirmadjour133@gmail.com>
2024-03-17 14:20:52 +0100

    Add Tailwindcss to code base

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,35 +1,15 @@
 import Versions from './components/Versions'
 import Flon from './components/Flon'
 import electronLogo from './assets/electron.svg'
+import "./index.css" 
 
 function App() {
   const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Flon/>
-      <Versions></Versions>
-    </>
+    <h1 className="text-3xl font-bold underline text-blue">
+      Hello world!
+    </h1>
   )
 }
 
