840aa1eb6f6eba9e939ae0435002e77c03702240
Mouhcine <mohciniou@hotmail.com>
2024-03-29 00:31:26 +0100

    Added possibility to add a user to my_table

--- Backend/ExpressApp.js
+++ Backend/ExpressApp.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+//const db = require('./config/db')
+const { InsertUser } = require('./config/db')
 const ExpressApp = express()
 
 ExpressApp.use(cors())
--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-unused-vars */
 const Database = require('better-sqlite3')
+
 const db = new Database('Backend/config/test.db')
 
 db.pragma('journal_mode = WAL')
@@ -15,7 +17,15 @@ const createTableQuery = `
 // Execute the SQL statement to create the table
 db.exec(createTableQuery)
 
-// Close the database connection
-db.close()
+// Function to add a user to the table "my_table"
+function InsertUser(id, name, age) {
+  id = Number(id)
+  name = String(name)
+  age = Number(age)
+  const existingUser = db.prepare(`SELECT * FROM my_table WHERE id = ?`).get(id)
+  if (existingUser) return true // User already exists
+  db.prepare(`INSERT INTO my_table (id, name, age) VALUES (?, ?, ?)`).run(id, name, age)
+  return false
+}
 
-module.exports = db
+module.exports = { db, InsertUser }
