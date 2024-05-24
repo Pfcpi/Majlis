379682ded5de81fc46895b993d13df4af4394ef7
Harkaso <oussammou3000@gmail.com>
2024-05-24 16:51:02 +0100

    bugfix with uppercase

--- Backend/routes/pv.js
+++ Backend/routes/pv.js
@@ -222,13 +222,13 @@ router.post('/addPV', (req, res) => {
               console.log(sent, 'temoins', temoin, ', if is an array : ', Array.isArray(temoin))
               temoin.map((t) => {
                 console.log(t)
-                db.query(sqlqueryT, [t.nomT.replace(/ /g, '\u00A0').toUppercase(), maj(t.prenomT.replace(/ /g, '\u00A0')), t.roleT], (err, result) => {
+                db.query(sqlqueryT, [t.nomT.replace(/ /g, '\u00A0').toUpperCase(), maj(t.prenomT.replace(/ /g, '\u00A0')), t.roleT], (err, result) => {
                   if (err) {
                     if (err.errno == 1062) {
                       console.log('tuple duplicated')
                       let getIndex =
                         'SELECT num_t FROM Temoin WHERE nom_t = ? AND prenom_t = ? AND role_t = ?'
-                      db.query(getIndex, [t.nomT.replace(/ /g, '\u00A0').toUppercase(), maj(t.prenomT.replace(/ /g, '\u00A0')), t.roleT], (err, result) => {
+                      db.query(getIndex, [t.nomT.replace(/ /g, '\u00A0').toUpperCase(), maj(t.prenomT.replace(/ /g, '\u00A0')), t.roleT], (err, result) => {
                         if (err) {
                           console.log(err)
                           temoinErrBuffer.push(err)
